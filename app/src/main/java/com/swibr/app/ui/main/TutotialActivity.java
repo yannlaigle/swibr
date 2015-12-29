cf155369704ee09b0e2f556b8aec05aaab955478
Harold Thetiot <hthetiot@gmail.com>
2015-12-29 23:15:23 +0100

    add git ignored file

---
+++ app/src/main/java/com/swibr/app/ui/main/TutotialActivity.java
@@ -0,0 +1,9 @@
+package com.swibr.app.ui.main;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by hthetiot on 12/29/15.
+ */
+public class TutotialActivity extends FragmentActivity {
+}
